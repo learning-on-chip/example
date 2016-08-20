921bf8aadb5c7fd737c36f653965e71f60a83f70
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-20 11:46:23 +0200

    Save the progress plots into a file

--- bin/learn.py
+++ bin/learn.py
@@ -5,19 +5,16 @@ sys.path.append(os.path.dirname(__file__))
 
 import matplotlib.pyplot as pp
 import numpy as np
-import support
+import subprocess, support, tempfile
 import tensorflow as tf
 
-def learn(f, dimension_count, sample_count, train_each, predict_each,
-          predict_count, predict_phases, epoch_count, train_monitor,
+def learn(f, dimension_count, sample_count, epoch_count, train_each,
+          train_monitor, predict_each, predict_count, predict_pace,
           predict_monitor):
 
-    n = sample_count // predict_each
-    while n > 0 and n*predict_each + predict_count > sample_count: n -= 1
-    sample_count = n*predict_each + predict_count
-    if n == 0: return
-
-    predict_phases = np.cumsum(predict_phases)
+    epoch_sample_count = sample_count - predict_count
+    epoch_sample_count -= epoch_sample_count % predict_each
+    predict_phases = np.cumsum(predict_pace)
 
     layer_count = 1
     unit_count = 20
@@ -45,9 +42,9 @@ def learn(f, dimension_count, sample_count, train_each, predict_each,
     session.run(initialize)
 
     parameter_count = np.sum([int(np.prod(p.get_shape())) for p in parameters])
-    print('Samples: %d' % sample_count)
     print('Parameters: %d' % parameter_count)
-    for k in range(epoch_count):
+    print('Epoch samples: %d' % epoch_sample_count)
+    for e in range(epoch_count):
         train_fetches = {'finish': finish, 'train': train, 'loss': loss}
         train_feeds = {
             start: np.zeros(start.get_shape(), dtype=np.float32),
@@ -59,32 +56,34 @@ def learn(f, dimension_count, sample_count, train_each, predict_each,
 
         Y = np.zeros([predict_count, dimension_count])
         Y_hat = np.zeros([predict_count, dimension_count])
-        for i, j in zip(range(sample_count - 1), range(1, sample_count)):
+        for s, t in zip(range(epoch_sample_count - 1),
+                        range(1, epoch_sample_count)):
+
             train_feeds[x] = np.roll(train_feeds[x], -1, axis=1)
             train_feeds[y] = np.roll(train_feeds[y], -1, axis=1)
-            train_feeds[x][0, -1, :] = f(i)
-            train_feeds[y][0, -1, :] = f(j)
+            train_feeds[x][0, -1, :] = f(s)
+            train_feeds[y][0, -1, :] = f(t)
 
-            if j % train_each == 0:
+            if t % train_each == 0:
                 train_results = session.run(train_fetches, train_feeds)
                 train_feeds[start] = train_results['finish']
-                train_monitor(progress=(k, j // train_each, j),
+                train_monitor(progress=(e, t // train_each, t),
                               loss=train_results['loss'].flatten())
 
-            phase = np.nonzero(predict_phases >= (i % predict_phases[-1]))[0][0]
-            if phase % 2 == 1 and j % predict_each == 0:
-                lag = j % train_each
+            phase = np.nonzero(predict_phases >= (s % predict_phases[-1]))[0][0]
+            if phase % 2 == 1 and t % predict_each == 0:
+                lag = t % train_each
                 predict_feeds[start] = train_feeds[start]
                 predict_feeds[x] = np.reshape(
                     train_feeds[y][0, (train_each - 1 - lag):, :],
                     [1, 1 + lag, -1])
-                for l in range(predict_count):
+                for i in range(predict_count):
                     predict_results = session.run(predict_fetches,
                                                   predict_feeds)
                     predict_feeds[start] = predict_results['finish']
-                    Y_hat[l, :] = predict_results['y_hat'][-1, :]
-                    predict_feeds[x] = np.reshape(Y_hat[l, :], [1, 1, -1])
-                    Y[l, :] = f(j + l + 1)
+                    Y_hat[i, :] = predict_results['y_hat'][-1, :]
+                    predict_feeds[x] = np.reshape(Y_hat[i, :], [1, 1, -1])
+                    Y[i, :] = f(t + i + 1)
                 predict_monitor(Y, Y_hat)
 
 def configure(dimension_count, layer_count, unit_count):
@@ -129,14 +128,17 @@ def configure(dimension_count, layer_count, unit_count):
                                 initializer=initializer)
             b = tf.get_variable('b', [1, dimension_count])
             y_hat = tf.matmul(x, w) + tf.tile(b, [unroll_count, 1])
-            loss = tf.reduce_sum(tf.square(tf.sub(y_hat, y)))
+            loss = tf.reduce_mean(tf.squared_difference(y_hat, y))
         return y_hat, loss
 
     return compute
 
+filename = os.path.join(tempfile.gettempdir(), 'learn.pdf')
+open(filename, 'w').close()
+print('Monitor: {}'.format(filename))
+subprocess.call(['open', filename])
 support.figure()
 y_limit = [-1, 1]
-pp.pause(1e-3)
 
 def train_monitor(progress, loss):
     sys.stdout.write('%4d %8d %10d' % progress)
@@ -155,18 +157,16 @@ def predict_monitor(y, y_hat):
         pp.xlim([0, y.shape[0] - 1])
         pp.ylim(y_limit)
         pp.legend(['Observed', 'Predicted'])
-    pp.pause(1e-3)
+    pp.savefig(filename)
 
 data = support.normalize(support.select(component_ids=[0]))
 learn(lambda i: data[i, :],
       dimension_count=data.shape[1],
       sample_count=data.shape[0],
-      train_each=50,
-      predict_each=5,
-      predict_count=100,
-      predict_phases=[10000, 1000],
       epoch_count=100,
+      train_each=50,
       train_monitor=train_monitor,
+      predict_each=10,
+      predict_count=100,
+      predict_pace=[863890 // 10 - 1000, 1000],
       predict_monitor=predict_monitor)
-
-pp.show()
